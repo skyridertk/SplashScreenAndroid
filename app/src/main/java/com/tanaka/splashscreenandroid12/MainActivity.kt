b13286de5b22402e55bcce13803de559cadfa2fd
Tanaka Ian Kahwai <iankahwai@gmail.com>
2022-01-03 14:30:39 +0200

    Initialize repo

---
+++ app/src/main/java/com/tanaka/splashscreenandroid12/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.tanaka.splashscreenandroid12
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.tanaka.splashscreenandroid12.ui.theme.SplashScreenAndroid12Theme
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepVisibleCondition {
+                viewModel.isLoading.value
+            }
+        }
+
+        setContent {
+            SplashScreenAndroid12Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SplashScreenAndroid12Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/tanaka/splashscreenandroid12/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.tanaka.splashscreenandroid12
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean>
+        get() = _isLoading
+
+
+    init {
+        viewModelScope.launch {
+            delay(3000)
+
+            _isLoading.value = false
+        }
+    }
+}
